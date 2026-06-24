3153eb9a90cf8c65421edcca65d86472dcc03103
yus-nand <nandivadekary24@gmail.com>
2026-06-24 14:37:35 +0530

    animation
    basic animation using image translation (without syncing it with logic)

--- Assets/Scripts/Core/ReelController.cs
+++ Assets/Scripts/Core/ReelController.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 
 public class ReelController : MonoBehaviour
 {
-    [SerializeField] private SymbolType[] symbols;
     public float timer;
+    public Transform[] reelImages;
     [HideInInspector] public int selectedSymbolIndex; 
+    [SerializeField] private SymbolType[] symbols;
+    private Vector3 stopPosition = Vector3.zero;
+    private bool shouldSpin = false;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
     public void Spin()
     {
         selectedSymbolIndex = Random.Range(0, symbols.Length);
@@ -14,7 +21,23 @@ public class ReelController : MonoBehaviour
     }
     IEnumerator ShowSymbol()
     {
+        shouldSpin = true;
         yield return new WaitForSeconds(timer);
+        shouldSpin = false;
         Debug.Log("Symbol: " + symbols[selectedSymbolIndex]);
+    }
+    void Update()
+    {
+        if(shouldSpin)
+            StartAnimation();
+    }
+    void StartAnimation()
+    {
+        foreach(var image in  reelImages)
+        {
+            image.Translate(20f * Vector3.down);
+            if(image.localPosition.y <= -150f)
+                image.localPosition = Vector3.up * 290;
+        }
     }
 }
